eb2b710373902c503ede69e1229912132c787ff8
DenisLjubojevic <denisljubojevic@gmail.com>
2024-11-27 12:52:22 +0100

    Updated JwtAuthFilter

--- backend/src/main/java/hr/tvz/ljubojevic/chatterbox/security/JwtAuthFilter.java
+++ backend/src/main/java/hr/tvz/ljubojevic/chatterbox/security/JwtAuthFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,7 +20,9 @@ import java.io.IOException;
 @Component
 @AllArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
     private final JwtService jwtService;
+    @Autowired
     private final CustomUserDetailsService customUserDetailsService;
 
 
