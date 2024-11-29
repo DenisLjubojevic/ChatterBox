eab9e86af003cbf6e205396714890be3f8498c1e
DenisLjubojevic <denisljubojevic@gmail.com>
2024-11-29 15:09:40 +0100

    Restructured userChatService

--- backend/src/main/java/hr/tvz/ljubojevic/chatterbox/service/userChat/UserChatServiceImpl.java
+++ backend/src/main/java/hr/tvz/ljubojevic/chatterbox/service/userChat/UserChatServiceImpl.java
@@ -2,28 +2,58 @@ package hr.tvz.ljubojevic.chatterbox.service.userChat;
 
 import hr.tvz.ljubojevic.chatterbox.model.ChatRoom;
 import hr.tvz.ljubojevic.chatterbox.model.User;
+import hr.tvz.ljubojevic.chatterbox.repository.jpa.ChatRoomRepository;
+import hr.tvz.ljubojevic.chatterbox.repository.jpa.MessageRepository;
 import hr.tvz.ljubojevic.chatterbox.repository.jpa.UserRepository;
-import hr.tvz.ljubojevic.chatterbox.service.chatRoom.ChatRoomService;
-import org.springframework.beans.factory.annotation.Autowired;
+import hr.tvz.ljubojevic.chatterbox.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class UserChatServiceImpl implements UserChatService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    public final ChatRoomRepository chatRoomRepository;
+    public final MessageRepository messageRepository;
+    public final FileStorageService fileStorageService;
 
-    @Autowired
-    private ChatRoomService chatRoomService;
+    @Value("${image.baseUrl}")
+    private String imageBaseUrl;
+
+    public UserChatServiceImpl(UserRepository userRepository,
+                               ChatRoomRepository chatRoomRepository,
+                               MessageRepository messageRepository,
+                               FileStorageService fileStorageService) {
+        this.userRepository = userRepository;
+        this.chatRoomRepository = chatRoomRepository;
+        this.messageRepository = messageRepository;
+        this.fileStorageService = fileStorageService;
+    }
 
     @Override
     @Transactional
     public User removeFriendAndDeleteChat(User user, User friend){
 
-        Optional<ChatRoom> chatRoomOpt = chatRoomService.findClosedChatRoomBetweenUsers(user.getUsername(), friend.getUsername());
-        chatRoomOpt.ifPresent(chatRoom -> chatRoomService.deleteChatRoomById(chatRoom.getId()));
+        Optional<ChatRoom> chatRoomOpt = chatRoomRepository.FindClosedChatRoomBetweenFriends(user.getUsername(), friend.getUsername());
+
+        chatRoomOpt.ifPresent(chatRoom -> {
+            Optional<ChatRoom> chatRoomOptional =  chatRoomRepository.findById(chatRoom.getId()).stream()
+                    .findFirst();
+            if (chatRoomOptional.isPresent()) {
+                this.messageRepository.deleteMsg(chatRoom.getId());
+
+                if (!Objects.equals(chatRoomOptional.get().getPictureUrl(), imageBaseUrl + "groupDefault.png")) {
+                    fileStorageService.deleteFile(chatRoomOptional.get().getPictureUrl());
+                }
+
+                chatRoomRepository.deleteById(chatRoom.getId());
+            }
+        });
+
+
 
         user.getFriends().remove(friend);
         friend.getFriends().remove(user);
