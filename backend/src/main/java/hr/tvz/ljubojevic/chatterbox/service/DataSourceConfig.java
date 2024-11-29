d3759d7f4b95ce52e8a869772bee1b386034bd4a
DenisLjubojevic <denisljubojevic@gmail.com>
2024-11-29 14:12:40 +0100

    Added DataSourceConfig

---
+++ backend/src/main/java/hr/tvz/ljubojevic/chatterbox/service/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package hr.tvz.ljubojevic.chatterbox.service;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        String databaseUrl = System.getenv("MYSQL_URL");
+        if (databaseUrl == null || databaseUrl.isEmpty()) {
+            throw new IllegalArgumentException("Environment variable DATABASE_URL is not set.");
+        }
+
+        URI dbURI = new URI(databaseUrl);
+        String userInfo = dbURI.getUserInfo();
+        String username = userInfo.split(":")[0];
+        String password = userInfo.split(":")[1];
+
+        String jdbcUrl = "jdbc:mysql://" + dbURI.getHost() + ":" + dbURI.getPort() + dbURI.getPath() +
+                "?useSSL=false&serverTimezone=UTC";
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        return dataSource;
+    }
+}
